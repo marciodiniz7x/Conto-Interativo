81d0841e8f1ca058c638288e9ec916d10a33b410
Márcio Diniz <marcio.sccp10@gmail.com>
2023-04-23 18:17:01 -0300

    Updates

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Seletores
+const capa = document.querySelector('.capa');
+const particles = document.querySelector('.particles');
 const titleDestaque = document.querySelector('.destacar-title');
 const titleText = document.querySelector('.title');
 const iniciarLeitura = document.querySelector('.iniciar-leitura');
 
 function iniciarCapa() {
     
-    titleDestaque.style.marginTop = "0px";
+
+    capa.classList.add('blur-surgir');
     setTimeout(() => {
-        titleText.style.marginLeft = "0px";
+        titleDestaque.style.marginTop = "0px";
         setTimeout(() => {
-            iniciarLeitura.classList.remove('sumir');
-            iniciarLeitura.classList.add('blur-surgir');
-        }, 600);
-    }, 400);
+            titleText.style.marginLeft = "0px";
+            
+            setTimeout(() => {
+                
+                particles.classList.replace("sumir","glitch");
+                iniciarLeitura.classList.remove('sumir');
+                iniciarLeitura.classList.add('blur-surgir');
+            }, 600);
+        }, 400);
+    }, 300);
     
 }
