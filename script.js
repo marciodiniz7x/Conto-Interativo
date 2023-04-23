7052bc22d9eb4f04a7baad1f40760773da10f2c5
Márcio Diniz <marcio.sccp10@gmail.com>
2023-04-23 18:39:55 -0300

    Updates

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const titleDestaque = document.querySelector('.destacar-title');
 const titleText = document.querySelector('.title');
 const iniciarLeitura = document.querySelector('.iniciar-leitura');
 
+const gooEyes = document.querySelector('.goo-eyes');
+
 function iniciarCapa() {
     
-
+    gooEyes.classList.add('piscar');
     capa.classList.add('blur-surgir');
     setTimeout(() => {
         titleDestaque.style.marginTop = "0px";
         setTimeout(() => {
             titleText.style.marginLeft = "0px";
-            
             setTimeout(() => {
-                
                 particles.classList.replace("sumir","glitch");
                 iniciarLeitura.classList.remove('sumir');
                 iniciarLeitura.classList.add('blur-surgir');
