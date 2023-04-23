895506d5c3bf73326195e705f0391e674907c984
Márcio Diniz <marcio.sccp10@gmail.com>
2023-04-23 18:53:11 -0300

    Updates

--- script.js
+++ script.js
@@ -5,11 +5,26 @@ const titleDestaque = document.querySelector('.destacar-title');
 const titleText = document.querySelector('.title');
 const iniciarLeitura = document.querySelector('.iniciar-leitura');
 
-const gooEyes = document.querySelector('.goo-eyes');
+// Criando os olhos de Goo na capa
+const gooEyes = document.createElement('div');
+gooEyes.classList.add('goo-eyes');
+
+const olho1 = document.createElement('div');
+olho1.classList.add('olho1', "eyes");
+
+const olho2 = document.createElement('div');
+olho2.classList.add('olho2', 'eyes');
+
+gooEyes.appendChild(olho1);
+gooEyes.appendChild(olho2);
+
+capa.appendChild(gooEyes);
 
 function iniciarCapa() {
     
+    // Adiciona animação para piscar os olhos
     gooEyes.classList.add('piscar');
+
     capa.classList.add('blur-surgir');
     setTimeout(() => {
         titleDestaque.style.marginTop = "0px";
